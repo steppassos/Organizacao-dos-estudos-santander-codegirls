5935514d46d7be972b957d52e2b19ec7129da379
Stephanie <steppassos@gmail.com>
2022-07-26 14:27:14 -0300

    Incluir pasta sobrecarga de Java

---
+++ Java/Sobrecargas/src/sobrecarga/dio/Main.java
@@ -0,0 +1,15 @@
+package sobrecarga.dio;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Quadrilatero
+        System.out.println("Exercicio Quadrilatero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5,5);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
+
+    }
+}
