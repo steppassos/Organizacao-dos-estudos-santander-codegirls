192aaa086a95707caaf92f327195c57abc90bb27
Stephanie <steppassos@gmail.com>
2022-08-08 13:45:33 -0300

    first commit

---
+++ Java/OperadoresRelacionais/Operadores Relacionais/src/operadores/relacionais/similaridadeetamanho.java
@@ -0,0 +1,18 @@
+package operadores.relacionais;
+
+public class similaridadeetamanho {
+
+    public static void main(String[] args) {
+
+        int i1 = 10;
+        int i2 = 20;
+        float f1 = 4.5f;
+        float f2 = 3.5f;
+        double d1 = 59.6d;
+        char c1 = 'x';
+        char c2 = 'y';
+        String s1 = "Fulano";
+        Strin
+
+    }
+}
